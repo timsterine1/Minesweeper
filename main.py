263cf2e14fc91c31dac512dbeab45e517bf46aa7
Tim Zimmer <timsterine@gmail.com>
2026-08-06 14:11:30 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+os.system("clear")
+
+class Block:
+    def __init__(self):
+        self.is_revealed = False
+        self.is_flaged = False
+        self.shown = False
+        self.is_bomb = False
+        self.neighbour_mine_number = 0
+        
+
+    #nicht zeichnen sondern nur die zahl wiedergeben die gezeichnet werden soll
+    def show(self)-> int: 
+        # wenn flag mode aktiviert ist int x y pos eingegeben wird dan 
+        if self.is_flaged == True:
+            # dan wird das zeichenwas ausgegeben wird auf 9 gesetzt 
+            return 9
+        elif self.is_bomb == False:
+            # dan wird das was ausgegeben wird mit einer function calculated
+            # berechnet und ausgegeben 
+            return self.get_neighbour_number()
+            
+        else: 
+             # dann ist es eine bombe und game over 
+             return -1
+
+    
+    def get_neighbour_number(self)-> int:
+        #eis wird gegugt wieviele bombem im umkreis sind 
+        #ein loop feur den 3x3 kasten
+        #vll mit -2 anfangen fuer x und y weil beim ersten durchkauf
+        #ist es dan -1 dan 0 und dan 1feur x und zusammen mit y
+        # ist es dan ein 3er grud wenn eine bombe da ist den zaheker um 1 hoch am edne wiedergeben 
+
+        return 0
+
+
+def get_int_input(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+                print("please enter a number")
+
+def main():
+        area_leng = get_int_input("how large should the grid be? ")
+        number_of_bombs = get_int_input("how many bombs do you want? ")
+
+        grid = [[Block() for _ in range(area_leng)] for _ in range(area_leng)]
+
+        for index in grid:
+                print([block.show() for block in index])
+
+main()
