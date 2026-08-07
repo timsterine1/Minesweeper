a549a071733321fd9435c2c91d13d2f9036f6c75
Tim Zimmer <timsterine@gmail.com>
2026-08-07 10:50:50 +0000

    Implement neighbour mine counting logic

--- main.py
+++ main.py
@@ -20,20 +20,35 @@ class Block:
         elif self.is_bomb == False:
             # dan wird das was ausgegeben wird mit einer function calculated
             # berechnet und ausgegeben 
-            return self.get_neighbour_number()
+            return self.neighbour_mine_number
             
         else: 
              # dann ist es eine bombe und game over 
              return -1
 
     
-    def get_neighbour_number(self)-> int:
-        #eis wird gegugt wieviele bombem im umkreis sind 
+    def get_neighbour_number(self,grid,xpos,ypos,area_leng)-> int:
+        #es wird geguckt wieviele bombem im umkreis sind 
         #ein loop feur den 3x3 kasten
         #vll mit -2 anfangen fuer x und y weil beim ersten durchkauf
         #ist es dan -1 dan 0 und dan 1feur x und zusammen mit y
-        # ist es dan ein 3er grud wenn eine bombe da ist den zaheker um 1 hoch am edne wiedergeben 
+        # ist es dan ein 3er grid wenn eine bombe da ist den zaheler um 1 hoch am edne wiedergeben
+        count = 0 
+        if grid[xpos][ypos].is_bomb == True:
+            return -1
+        for row in range(3):
+            for collum in range(3):
+                nx = xpos - 1 + row
+                ny = ypos - 1 + collum
 
+                if 0 <= nx < area_leng and 0 <= ny < area_leng:
+                    if grid[nx][ny].is_bomb:
+                        count += 1
+
+        self.neighbour_mine_number = count
+        
+
+                  
         return 0
 
 
@@ -60,6 +75,9 @@ def main():
                      grid[random_x][random_y].is_bomb = True
                      count += 1
         place_bombs()
+        for i in range(area_leng):
+             for j in range(area_leng):
+                grid[i][j].get_neighbour_number(grid,i,j,area_leng) 
         for index in grid:
                 print([block.show() for block in index])
 
